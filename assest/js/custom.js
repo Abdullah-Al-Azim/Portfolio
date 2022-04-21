1b6e6db50d3c5d2ede0efa00bedf692323786ffa
Abdullah-Al-Azim <azeemzeehan3@gmail.com>
2022-04-21 17:18:28 +0600

    4/21//22

--- assest/js/custom.js
+++ assest/js/custom.js
@@ -3,3 +3,12 @@ $(document).ready(function(){
     $('.tooltipped').tooltip();
   });
         
+
+// typed js
+
+var typed = new Typed(".auto___typed", {
+  strings: ["human", "traveller", "music lover"],
+  typeSpeed : 150,
+  backSpeed : 50,
+  loop: true
+})
